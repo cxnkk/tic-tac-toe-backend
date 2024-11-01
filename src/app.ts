06aea62ba89903b90940df085a1e73dbfc46e30a
cxnkk <cenkoezkartal@gmail.com>
2024-11-01 13:27:44 +0100

    done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { data } from "./data";
 
-const app = express();
+export const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/data", (req, res) => {
   res.send(data);
