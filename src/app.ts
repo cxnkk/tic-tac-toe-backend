e62bb388fc2ff7ce0331cfb2e40421c2a78c6553
cxnkk <cenkoezkartal@gmail.com>
2025-01-20 19:36:22 +0100

    added data and first endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { data } from "./data";
+import { players } from "./data";
 
 export const app = express();
 const port = 3000;
@@ -8,8 +8,22 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/data", (req, res) => {
-  res.send(data);
+app.get("/tictactoe", (req, res) => {
+  res.send(players);
+});
+
+app.put("/tictactoe/playing", (req, res) => {
+  const { userId } = req.body;
+
+  let findPlayer = players.find((id) => {
+    return id.userId === userId;
+  });
+
+  if (!findPlayer) {
+    return res.sendStatus(404);
+  }
+
+  return res.send(findPlayer.playingSymbol).status(200);
 });
 
 app.listen(port, () => {
